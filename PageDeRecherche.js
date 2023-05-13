ccfde4c9f418440b2d8bc48a381a552ab93d6d1d
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-05-13 23:25:01 +0100

    adding styles

--- PageDeRecherche.js
+++ PageDeRecherche.js
@@ -23,11 +23,17 @@ export default class PageDeRecherche extends Component<Props>{
 
 const styles = StyleSheet.create({
     description: {
+        marginBottom: 20,
         fontSize: 18,
         textAlign: 'center',
         color: '#656565',
-        marginTop: 65,
     },
+    conteneur: {
+        padding:30,
+        marginTop: 65,
+        alignItems: 'center'
+
+    }
 });
 
 
