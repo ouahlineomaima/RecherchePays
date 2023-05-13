1d7165ccd34ab025578581d88c8d37e6e2e5b187
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-05-13 23:21:23 +0100

    code sans JSX

--- PageDeRecherche.js
+++ PageDeRecherche.js
@@ -1,40 +1,35 @@
-import 'react-native-gesture-handler'
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Button,
-  ActivityIndicator,
-  Image,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    Button,
+    ActivityIndicator,
+    Image,
 } from 'react-native';
 
 type Props = {};
 export default class PageDeRecherche extends Component<Props>{
-  render(){
-    return (
-        <View style={styles.conteneur}>
-            <Text style ={styles.description}>
-                Rechercher des pays à explorer!
-            </Text>
-            <Text style ={styles.description}>
-                Rechercher par nom
-            </Text>
-        </View>     
-    );
-  }
+    render() {
+        return (
+            React.createElement(View, { style: styles.conteneur },
+                React.createElement(Text, { style: styles.description }, "Rechercher des pays à explorer!"),
+                React.createElement(Text, { style: styles.description }, "Rechercher par nom")
+            )
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     description: {
-      fontSize: 18,
-      textAlign: 'center',
-      color: '#656565',
-      marginTop: 65,
+        fontSize: 18,
+        textAlign: 'center',
+        color: '#656565',
+        marginTop: 65,
     },
-  });
-  
+});
+
 
 
 
