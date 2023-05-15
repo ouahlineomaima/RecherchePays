2ce2e848fa6739299d16cb0eff1cd911e1cd2703
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-05-15 02:18:09 +0100

    notation fonctionnelle

--- PageDeRecherche.js
+++ PageDeRecherche.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import {StyleSheet,
+import React, { useState } from 'react';
+import {
+    StyleSheet,
     Text,
     TextInput,
     View,
@@ -7,69 +8,72 @@ import {StyleSheet,
     ActivityIndicator,
     Image,
 } from 'react-native';
-function urlPourRequete(valeur){
-    return 'https://restcountries.com/v3.1/name/'+valeur;
-}
-type Props = {};
-export default class PageDeRecherche extends Component<Props>{
-    constructor(props){
-        super(props);
-        this.state={
-            requeteDeRecherche: 'morocco',
-            estEnChangement: false,
-            message: '',
-        };
-    }
-    _auchangementDeLaRecherche = (event) =>{
-        this.setState({requeteDeRecherche: event.nativeEvent.text});
-    }
-    _gererLaReponse = (reponse) =>{
-        this.setState({estEnChangement: false, message: ''});
-        console.log('Nombre de pays trouvés: ' + reponse.length);
-    }
-    _executerRequete = (requete) =>{
-        console.log(requete);
-        this.setState({estEnChangement: true});
-        fetch(requete)
-        .then(response => response.json())
-        .then(json => this._gererLaReponse(json))
-        .catch(error => this.setState({estEnChangement: false, message: 'Quelque chose de mauvais s\'est produit ' + error}))
-    };
-    _auDemarrageDeLaRecherche = () =>{
-        /* const requete = urlPourRequete(this.state.requeteDeRecherche); */
-        const requete = 'https://restcountries.com/v3.1/name/' + this.state.requeteDeRecherche
-        this._executerRequete(requete);
-    }
-    render() {
 
-        return (
-            <View style={styles.conteneur}>
-                <Text style={styles.description}>
-                    Rechercher des pays à explorer!
-                </Text>
-                <Text style={styles.description}>
-                    Rechercher par nom
-                </Text>
-                <View style={styles.fluxDroite}>
-                    <TextInput 
+const urlPourRequete = (valeur) => {
+    return 'https://restcountries.com/v3.1/name/' + valeur;
+};
+
+export default function PageDeRecherche() {
+    const [requeteDeRecherche, setRequeteDeRecherche] = useState('morocco');
+    const [estEnChangement, setEstEnChangement] = useState(false);
+    const [message, setMessage] = useState('');
+
+    const auchangementDeLaRecherche = (event) => {
+        setRequeteDeRecherche(event.nativeEvent.text);
+    };
+
+    const gererLaReponse = (reponse) => {
+        setEstEnChangement(false);
+        setMessage('');
+        console.log('Nombre de pays trouvés: ' + reponse.length);
+    };
+
+    const executerRequete = (requete) => {
+        console.log(requete);
+        setEstEnChangement(true);
+        fetch(requete)
+            .then((response) => response.json())
+            .then((json) => gererLaReponse(json))
+            .catch(
+                (error) =>
+                    setMessage("Quelque chose de mauvais s'est produit " + error),
+                setEstEnChangement(false)
+            );
+    };
+
+    const auDemarrageDeLaRecherche = () => {
+        /* const requete = urlPourRequete(requeteDeRecherche); */
+        const requete =
+            'https://restcountries.com/v3.1/name/' + requeteDeRecherche;
+        executerRequete(requete);
+    };
+
+    return (
+        <View style={styles.conteneur}>
+            <Text style={styles.description}>Rechercher des pays à explorer!</Text>
+            <Text style={styles.description}>Rechercher par nom</Text>
+            <View style={styles.fluxDroite}>
+                <TextInput
                     underlineColorAndroid={'transparent'}
                     style={styles.requeteEntree}
-                    value={this.state.requeteDeRecherche}
-                    onChange={this._auchangementDeLaRecherche}
-                    placeholder='Rechercher par nom de pays'/>
-                    <Button 
-                    onPress={this._auDemarrageDeLaRecherche}
+                    value={requeteDeRecherche}
+                    onChange={auchangementDeLaRecherche}
+                    placeholder="Rechercher par nom de pays"
+                />
+                <Button
+                    onPress={auDemarrageDeLaRecherche}
                     color={'#48AAEC'}
-                    title='Démarrer'
-                    />
-                </View>
-                <Image source={require('./Ressources/pays.jpg')} style={styles.image}/>
-                <Text style={styles.description}>{this.state.message}</Text>
+                    title="Démarrer"
+                />
             </View>
-        );
-    }
+            <Image
+                source={require('./Ressources/pays.jpg')}
+                style={styles.image}
+            />
+            <Text style={styles.description}>{message}</Text>
+        </View>
+    );
 }
-
 
 const styles = StyleSheet.create({
     description: {
@@ -81,15 +85,14 @@ const styles = StyleSheet.create({
     conteneur: {
         padding: 30,
         marginTop: 65,
-        alignItems: 'center'
-
+        alignItems: 'center',
     },
-    fluxDroite:{
+    fluxDroite: {
         flexDirection: 'row',
         alignItems: 'center',
         alignSelf: 'stretch',
     },
-    requeteEntree:{
+    requeteEntree: {
         height: 36,
         padding: 4,
         marginRight: 5,
@@ -100,13 +103,8 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         color: '#48AAEC',
     },
-    image:{
+    image: {
         width: 220,
         height: 140,
-    }
+    },
 });
-
-
-
-
-
